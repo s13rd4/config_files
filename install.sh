682c70cc92548abe240eccbaa08e32356f937762
s13rd4 <lorenzobass97@gmail.com>
2021-09-10 17:39:12 +0200

    fixed installer bugs

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@
 set -e
 
 paruinstall() {
-	sudo pacman -S --needed base-devel && \
+	
+	sudo pacman -S git && \
+		sudo pacman -S --needed base-devel && \
 		git clone https://aur.archlinux.org/paru.git && \
 		cd paru && \
 		makepkg -si && \
+		cd .. && \
 		rm -rf paru
 }
 
@@ -26,6 +29,10 @@ zsh_conf() {
 	
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && \
 	chsh -s /bin/zsh
+	if [ -d $HOME/.oh-my-zsh ]
+		then
+		mv $HOME/.oh-my-zsh $HOME/.config/oh-my-zsh;
+	fi
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.config/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-$HOME/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-history-substring-search.git ${ZSH_CUSTOM:-$HOME/.config/oh-my-zsh/custom}/plugins/zsh-history-substring-search
@@ -42,7 +49,7 @@ pkgs() {
 }
 
 
-echo "INSTALLING AUR HELPER AND PACKAGES" && pkgs;
+echo "INSTALLING AUR HELPER AND PACKAGES" && paruinstall && pkgs;
 echo "STOWING CONFIG FILES" && stow_conf;
 echo "INSTALLING ZSH PLUGINS" && zsh_conf;
 echo "INSTALLING TMUX PACKAGES" && tmux_conf;
