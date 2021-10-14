1a811e2938d245b8b085a7e95fb5c217aae500c8
s13rd4 <lorenzobass97@gmail.com>
2021-10-14 14:44:03 +0200

    started git integration

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -1,33 +1,39 @@
 return {
 	-- packer auto managing
-	{ "wbthomason/packer.nvim" },
-
-	{ "neovim/nvim-lspconfig" },
-	{ "kabouzeid/nvim-lspinstall" },
-    { "nvim-treesitter/nvim-treesitter",
+	{"wbthomason/packer.nvim"},
+	{"neovim/nvim-lspconfig"},
+	{"kabouzeid/nvim-lspinstall"},
+    {"nvim-treesitter/nvim-treesitter",
       run = ":TSUpdate"
     },
-	{ "hrsh7th/nvim-cmp",
+	{"hrsh7th/nvim-cmp",
 	  requires = {
-		  "hrsh7th/vim-vsnip",
-		  "hrsh7th/cmp-buffer",
-		  "hrsh7th/cmp-vsnip",
-		  "hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/vim-vsnip",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-vsnip",
+		"hrsh7th/cmp-nvim-lsp",
 	  }
 	},
-	{ 'kyazdani42/nvim-web-devicons'},
-	{ 'nvim-telescope/telescope.nvim',
+	{'kyazdani42/nvim-web-devicons'},
+	{'nvim-telescope/telescope.nvim',
 	  requires = {
-		  'nvim-lua/popup.nvim',
-		  'nvim-lua/plenary.nvim',
-		  'nvim-telescope/telescope-fzy-native.nvim'
+		'nvim-lua/popup.nvim',
+		'nvim-lua/plenary.nvim',
+		'nvim-telescope/telescope-fzy-native.nvim'
 	  }
     },
-
-
-	{ "marko-cerovac/material.nvim"},
+	{'tpope/vim-fugitive'},
+	{'lewis6991/gitsigns.nvim',
+	  requires = {
+		'nvim-lua/plenary.nvim'
+	  },
+	  config = function()
+		  require('gitsigns').setup()
+	  end
+	},
+	{"marko-cerovac/material.nvim"},
 	{"aserowy/tmux.nvim",
-		config = function()
+	  config = function()
 			require("tmux").setup({
 				copy_sync = {
 					-- enables copy sync
@@ -39,6 +45,6 @@ return {
 					enable_default_keybindings = false,
 				}
 			})
-		end
+	  end
 	}
 }
