9bbf4a15839196805cac357e3a61618599d70dc9
s13rd4 <lorenzobass97@gmail.com>
2021-10-29 17:51:44 +0200

    started pyright integration

--- neovim/.config/nvim/ftplugin/python.lua
+++ neovim/.config/nvim/ftplugin/python.lua
@@ -2,7 +2,29 @@ local function on_attach()
 
 end
 
+local util = require('lspconfig').util
+
 require('lspconfig').python.setup({
 	on_attach = on_attach,
 	capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+	root_dir = function(fname)
+				 local root_files = {
+					 'pyproject.toml',
+					 'setup.py',
+					 'setup.cfg',
+					 'requirements.txt',
+					 'Pipfile',
+					 'pyrighitconfig.json',
+				 }
+				 return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+			   end,
+	settings = {
+	  python = {
+		analysis = {
+		  autoSearchPaths = true,
+		  diagnosticMode = "workspace",
+		  useLibraryCodeForTypes = true,
+		}
+	  }
+	}
 })
