31906a36d8c99195ee3fb1344111b22f1ec98a2d
Lorenzo Argentieri <lorenzobass97@gmail.com>
2021-11-07 19:39:52 +0100

    added git branches and git push to mappings

--- neovim/.config/nvim/lua/mappings/git.lua
+++ neovim/.config/nvim/lua/mappings/git.lua
@@ -5,4 +5,5 @@ local defaults = { noremap = true, silent = true }
 map('n','<leader>gf' ,'<CMD>diffget //2<CR>',defaults)
 map('n','<leader>gj' ,'<CMD>diffget //3<CR>',defaults)
 map('n','<leader>gs' ,'<CMD>:G<CR>',defaults)
-map('n','<leader>gb' ,'<CMD>:GBranches<CR>',defaults)
+map('n','<leader>gb' ,'<CMD>:Git branch<CR>',defaults)
+map('n','<leader>gp' ,'<CMD>:Git push<CR>',defaults)
