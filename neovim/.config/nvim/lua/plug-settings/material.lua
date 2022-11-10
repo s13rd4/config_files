1179c95f4906597236976c93626a81aaa7088c6a
Lorenzo Argentieri <lorenzobass97@gmail.com>
2022-11-10 11:43:13 +0100

    Fixed breaking changes in material.nvim

--- neovim/.config/nvim/lua/plug-settings/material.lua
+++ neovim/.config/nvim/lua/plug-settings/material.lua
@@ -1,32 +1,47 @@
 require('material').setup({
 
 	contrast = {
+		terminal = false,
+		floating_windows = false,
 		sidebars = false,
-		popup_menu = false
+		cursor_line = false,
+		non_current_windows = false,
+		filetypes = {
+			"qf",
+			"terminal",
+			"packer"
+		},
 	},
-
-	italics = {
-		comments = false, 
-		keywords = false, 
-		functions = false, 
-		strings = false, 
-		variables = false 
+	styles = {
+		comments = {italic=true},
+		strings = {},
+		keywords = {bold=true},
+		functions = {bold=true},
+		variables = {},
+		operators = {},
+		types = {},
 	},
-
-	contrast_filetypes = { 
-		"terminal", 
-		"packer", 
-		"qf" 
-	},
-
-	high_visibility = {
-		lighter = true, 
-		darker = true 
+	plugins = {
+		"telescope",
+		"nvim-web-devicons",
+		"gitsigns",
+		"nvim-cmp",
 	},
 
 	disable = {
-		background = false, 
-		term_colors = false, 
-		eob_lines = false 
-	}
+		background = false,
+		term_colors = false,
+		eob_lines = false
+	},
+
+	high_visibility = {
+		lighter = true,
+		darker = true
+	},
+	lualine_style = "default",
+	async_loading = true,
+
+    custom_colors = nil,
+
+    custom_highlights = {},
 })
