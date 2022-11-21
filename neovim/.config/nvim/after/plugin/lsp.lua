2d18afb7059941f89ccb69ada14cdfcad481083c
Lorenzo Argentieri <lorenzobass97@gmail.com>
2022-11-21 15:56:02 +0100

    Changed lsp configuration

--- neovim/.config/nvim/after/plugin/lsp.lua
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -2,12 +2,17 @@ local Remap = require("sierda.keymap")
 local nnoremap = Remap.nnoremap
 local inoremap = Remap.inoremap
 
-local sumneko_root_path = vim.env.HOME .. '/.local/share/nvim/lsp_servers/sumneko_lua/extension/server'
-local sumneko_bin = sumneko_root_path .. '/bin/lua-language-server'
-
 local function config(_config)
 	return vim.tbl_deep_extend("force", {
-		on_attach = function(client)
+		on_attach = function()
+			--local function bnnoremap(...)
+				--Remap.bnnoremap(bufnr, ...)
+			--end
+--
+			--local function binoremap(...)
+				--Remap.binoremap(bufnr, ...)
+			--end
+
 			nnoremap("gD", function() vim.lsp.buf.declaration() end)
 			nnoremap("gd", function() vim.lsp.buf.definition() end)
 			nnoremap("gi", function() vim.lsp.buf.implementation() end)
@@ -19,37 +24,34 @@ local function config(_config)
 			nnoremap("]d", function() vim.diagnostic.goto_prev() end)
 			nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
 			nnoremap("<leader>vco", function() vim.lsp.buf.code_action({
-                filter = function(code_action)
-                    if not code_action or not code_action.data then
-                        return false
-                    end
+					filter = function(code_action)
+						if not code_action or not code_action.data then
+							return false
+						end
 
-                    local data = code_action.data.id
-                    return string.sub(data, #data - 1, #data) == ":0"
-				end,
-                apply = true
-            })
+						local data = code_action.data.id
+						return string.sub(data, #data - 1, #data) == ":0"
+					end,
+					apply = true })
 			end)
 			nnoremap("<leader>vrr", function() vim.lsp.buf.references() end)
 			nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
 			inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
-			if client.server_capabilities.document_formatting then
-				nnoremap("ff",function() vim.lsp.buf.formatting() end)
-			elseif client.server_capabilities.document_range_formatting then
-				nnoremap("ff",function() vim.lsp.buf.range_formatting() end)
-			end
+			nnoremap("<leader>f", function() vim.lsp.buf.format() end)
 		end,
-	},_config or {})
+	}, _config or {})
 end
 
+local sumneko_root_path = vim.env.HOME .. '/.local/share/nvim/lsp_servers/sumneko_lua/extension/server'
+local sumneko_bin = sumneko_root_path .. '/bin/lua-language-server'
 require("lspconfig").sumneko_lua.setup(config({
 	cmd = { sumneko_bin, "-E", sumneko_root_path .. "/main.lua" },
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
 	settings = {
 		Lua = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
 				version = "LuaJIT",
-				-- Setup your lua path
 				path = vim.split(package.path, ";"),
 			},
 			diagnostics = {
@@ -58,13 +60,55 @@ require("lspconfig").sumneko_lua.setup(config({
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-				},
+				library = vim.api.nvim_get_runtime_file("", true)
 			},
+			telemetry = {
+				enable = false,
+			}
 		},
 	},
 }))
 
 
+local goplsbin = vim.env.HOME .. '/.local/share/nvim/lsp_servers/go/gopls'
+require('lspconfig').gopls.setup(config({
+	cmd = { goplsbin, 'serve' },
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
+	settings = {
+		gopls = {
+			analyses = {
+				unusedparams = true,
+				shadow = true,
+			},
+			staticcheck = true,
+		},
+	}
+}))
+
+
+local util = require('lspconfig').util
+local pyrightbin = vim.env.HOME .. '/.local/share/nvim/lsp_servers/python/node_modules/.bin/pyright-langserver'
+require('lspconfig').pyright.setup(config({
+	cmd = { pyrightbin, "--stdio" },
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
+	root_dir = function(fname)
+		local root_files = {
+			'pyproject.toml',
+			'setup.py',
+			'setup.cfg',
+			'requirements.txt',
+			'Pipfile',
+			'pyrighitconfig.json',
+		}
+		return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname) or util.path.dirname(fname)
+	end,
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "workspace",
+				useLibraryCodeForTypes = true,
+			}
+		}
+	}
+}))
--- neovim/.config/nvim/lua/sierda/keymap.lua
+++ neovim/.config/nvim/lua/sierda/keymap.lua
@@ -1,18 +1,37 @@
 local M = {}
 
-local function bind(op,outer_opts)
-	outer_opts = outer_opts or {noremap = true}
-	return function(lhs,rhs,opts)
+local function bind(op, outer_opts)
+	outer_opts = outer_opts or { noremap = true }
+	return function(lhs, rhs, opts)
 		opts = vim.tbl_exetend("force",
 			outer_opts,
 			opts or {}
 		)
-		vim.keymap.set(op,lhs,rhs,opts)
+		vim.keymap.set(op, lhs, rhs, opts)
+	end
+end
+
+local function buffer_bind(op, outer_opts)
+	outer_opts = outer_opts or { noremap = true }
+	return function(buf,lhs, rhs, opts)
+		opts = vim.tbl_exetend("force",
+			outer_opts,
+			opts or {}
+		)
+		vim.api.nvim_buf_set_keymap(buf,op,lhs,rhs,opts)
 	end
 end
 
 
-M.nmap = bind("n",{noremap = false})
+M.bnmap = buffer_bind("n", { noremap = false })
+M.bnnoremap = buffer_bind("n")
+M.bvnoremap = buffer_bind("v")
+M.bxnoremap = buffer_bind("x")
+M.binoremap = buffer_bind("i")
+M.btnoremap = buffer_bind("t")
+M.bcnoremap = buffer_bind("c")
+
+M.nmap = bind("n", { noremap = false })
 M.nnoremap = bind("n")
 M.vnoremap = bind("v")
 M.xnoremap = bind("x")
