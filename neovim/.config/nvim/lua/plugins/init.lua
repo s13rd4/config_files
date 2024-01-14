4abee6a6a3b1efdb32769e21160a615e45008244
Lorenzo <cash@MacBook-Pro.technicolor.net>
2024-01-14 23:31:09 +0100

    formatted init.lua

--- neovim/.config/nvim/lua/plugins/init.lua
+++ neovim/.config/nvim/lua/plugins/init.lua
@@ -1,18 +1,37 @@
 return {
-	{ 'tpope/vim-sleuth' },
-	{ 'folke/neodev.nvim',
-		opts = {},
-	},
-	{ 'theprimeagen/harpoon' },
-	{ -- Add indentation guides even on blank lines
-		'lukas-reineke/indent-blankline.nvim',
-		-- Enable `lukas-reineke/indent-blankline.nvim`
-		-- See `:help ibl`
-		main = 'ibl',
-		opts = {},
-	},
-	{ 'numToStr/Comment.nvim',
-		opts = {},
-		lazy = false
-	},
+  { 'tpope/vim-sleuth' },
+  {
+    'folke/neodev.nvim',
+    opts = {},
+  },
+  { 'theprimeagen/harpoon' },
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
+  {
+    'numToStr/Comment.nvim',
+    opts = {},
+    lazy = false,
+  },
+  {
+    'kevinhwang91/nvim-bqf',
+    ft = 'qf',
+    config = true
+  },
+  {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {}
+    end,
+  },
+  {
+    'akinsho/toggleterm.nvim',
+    config = true,
+  },
 }
