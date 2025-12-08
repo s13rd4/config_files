f44d783c74193c4761358f917da1013a85be0fa3
Lorenzo <cash@MacBook-Pro.local>
2025-12-08 17:01:24 +0100

    added sources to none-ls conf

--- neovim/.config/nvim/lua/plugins/none-ls.lua
+++ neovim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,13 +1,17 @@
 return {
-	'nvimtools/none-ls.nvim',
-	config = function()
-		local null_ls = require 'null-ls'
-		null_ls.setup {
-			sources = {
-				null_ls.builtins.formatting.stylua,
-			},
-		}
+  'nvimtools/none-ls.nvim',
+  config = function()
+    local null_ls = require 'null-ls'
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.code_actions.gitsigns,
+				null_ls.builtins.completion.luasnip,
+				null_ls.builtins.diagnostics.luacheck,
+				null_ls.builtins.diagnostics.golangci_lint,
+      },
+    }
 
-		vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set('n', '<leader>fo', vim.lsp.buf.format, {})
+  end,
 }
