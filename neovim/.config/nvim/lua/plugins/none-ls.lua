436aeb52720e892105b299122fce596fde2ec384
Lorenzo <cash@MacBook-Pro.local>
2025-12-08 17:02:16 +0100

    removed luacheck

--- neovim/.config/nvim/lua/plugins/none-ls.lua
+++ neovim/.config/nvim/lua/plugins/none-ls.lua
@@ -6,9 +6,8 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.code_actions.gitsigns,
-				null_ls.builtins.completion.luasnip,
-				null_ls.builtins.diagnostics.luacheck,
-				null_ls.builtins.diagnostics.golangci_lint,
+        null_ls.builtins.completion.luasnip,
+        null_ls.builtins.diagnostics.golangci_lint,
       },
     }
 
