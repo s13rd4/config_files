7bd7996911fec209130d273e003caef5bcec5279
s13rd4 <lorenzobass97@gmail.com>
2026-06-17 01:22:05 +0200

    hide uuid and enable toggle

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -7,7 +7,16 @@ return {
 		'MunifTanjim/nui.nvim',
 	},
 	config = function()
-		vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', { desc = 'Reveal file tree' })
+		vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', { desc = 'Reveal file tree' })
 		vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', { desc = 'Buffers (floating tree)' })
+		require('neo-tree').setup({
+			filesystem = {
+				filtered_items = {
+					visible = false,
+					hide_gitignored = true,
+					hide_dotfiles = true,
+				}
+			}
+		})
 	end,
 }
