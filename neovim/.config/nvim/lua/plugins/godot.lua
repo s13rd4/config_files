b2a0aab5db278363a21c887b39633e92a7f4108c
s13rd4 <lorenzobass97@gmail.com>
2026-06-17 01:14:44 +0200

    added godotdev plugin

--- neovim/.config/nvim/lua/plugins/godot.lua
+++ neovim/.config/nvim/lua/plugins/godot.lua
@@ -1,9 +1,19 @@
 return {
-  { "habamax/vim-godot" },
-  { "skywind3000/asyncrun.vim" },
-  { "teatek/gdscript-extended-lsp.nvim", opts = { view_type = "floating", picker = "snacks" } },
+  { 'habamax/vim-godot' },
+  { 'skywind3000/asyncrun.vim' },
   {
-    "folke/snacks.nvim",
+    'Mathijs-Bakker/godotdev.nvim',
+    dependencies = { 'mfussenegger/nvim-dap', 'rcarriga/nvim-dap-ui', 'nvim-treesitter/nvim-treesitter' },
+    config = function ()
+      require('godotdev').setup({
+        csharp = false,
+
+      })
+    end
+  },
+  { 'teatek/gdscript-extended-lsp.nvim', opts = { view_type = 'floating', picker = 'snacks' } },
+  {
+    'folke/snacks.nvim',
     opts = {
       picker = {
         sources = {
@@ -11,8 +21,8 @@ return {
             hidden = true, -- show hidden files
             ignored = false, -- don't show gitignored files
             exclude = { -- exclude specific patterns
-              "*.uid", -- glob pattern for files ending with .uid
-              "server.pipe", -- exact filename match
+              '*.uid', -- glob pattern for files ending with .uid
+              'server.pipe', -- exact filename match
             },
           },
         },
